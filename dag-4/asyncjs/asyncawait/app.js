6534e959ae37703261230fe33cec6ea831e0f333
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2022-01-04 14:15:36 +0100

    Updated Day-4 with async await vehicles

--- dag-4/asyncjs/asyncawait/app.js
+++ dag-4/asyncjs/asyncawait/app.js
@@ -6,8 +6,16 @@ const displayView = document.querySelector('#display');
 
 // Lägg till händelser...
 buttonLoadText.addEventListener('click', getText);
+buttonLoadJson.addEventListener('click', buttonLoadJsonData);
 
 // Skapa funktioner...
+
+// Starta hämtning av json data...
+async function buttonLoadJsonData() {
+  const vehicles = await getJson();
+  renderHtml(vehicles);
+}
+
 async function getText() {
   // Fetch Api...
   // Promise baserad...
@@ -26,4 +34,35 @@ async function getText() {
   // Hämta data(texten) ifrån response objektets body...
   const text = await response.text();
   displayView.innerHTML = text;
+}
+
+async function getJson() {
+  // Fetch Api...
+  // Promised baserad...
+  // fetch('vehicles.json').then((response) => {
+  //   console.log(response);
+  //   response.json().then((data) => {
+  //     console.log(data);
+  //     data.forEach((vehicle) => {
+  //       console.log(vehicle.make);
+  //     });
+  //   });
+  // });
+
+  // Async Await baserat...
+  const response = await fetch('vehicles.json');
+
+  if (!response.ok) throw new Error(response.statusText);
+
+  // Plocka ut json datat ur response body...
+  return await response.json();
+}
+
+function renderHtml(vehicles) {
+  let html = '';
+  vehicles.forEach((vehicle) => {
+    html += `<div>${vehicle.registrationNo} <span>${vehicle.make}</span><span> ${vehicle.model}</span></div>`;
+  });
+
+  displayView.innerHTML = html;
 }
