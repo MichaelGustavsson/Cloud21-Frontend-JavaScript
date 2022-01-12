6b33f5bd492d59b76fe4903edb03f5d4c2f8ef60
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2022-01-12 11:22:13 +0100

    Update day-6 added remove possibility for vehicles

--- dag-6/scripts/admin.js
+++ dag-6/scripts/admin.js
@@ -5,6 +5,9 @@ const vehicleTable = document.querySelector('#vehicle-list');
 function createTable(vehicleList) {
   let html = '';
 
+  //Töm <tbody></tbody> på dess data...
+  vehicleTable.innerHTML = '';
+
   /* Skapa tabellen dynamiskt */
   for (let car of vehicleList) {
     html += `
@@ -26,62 +29,38 @@ function createTable(vehicleList) {
 
   /* Slut på skapandet av tabellen */
 
-  //Iterera igenom alla rader och plocka ut kolumnen för redigering...
-  // Hämta in ALLA rader och returnera alla kolumner som har css klassen .edit under klassen .table-container...
   let tableRows = document.querySelectorAll('.table-container .edit');
-  // console.log(tableRows);
 
   tableRows.forEach((item) => {
     // Traversera dom för att få tag i identiteten för varje bil...
-    // console.log(item);
-    // console.log(item.parentNode);
-    // console.log(item.parentNode.parentNode);
-    // console.log(item.parentNode.parentNode.children);
-    // console.log(item.parentNode.parentNode.children[1]);
-    // console.log(item.parentNode.parentNode.children[1].firstChild.nodeValue);
-
-    //Utgå ifrån <i></i> gå hela vägen upp till <tr></tr> sedan hämta 2:a barn elementet vilket är <td></td> för id. Hämtar ut första barnets värdet.
-    // const id = item.parentNode.parentNode.children[1].firstChild.nodeValue;
     const id = item.parentNode.parentNode.children[1].firstChild.nodeValue;
-    // console.log(id);
+
     // Skapa en klick händelse på ikonen penna...
     item.addEventListener('click', () => {
-      // console.log(`Du klickade på bil med identitet ${id}`);
-      // Navigera till en ny sida med hjälp av webbläsarens location api.
-      // Skapa en frågesträng(querystring) ? lägg till en variabel(vehicleId)
-      // och sätt variabelns värde till det hämtade värdet ur item.
-      // console.log(`Jag klickade på bilen med id ${id}`);
       location.href = `edit.html?vehicleId=${id}`;
     });
+  });
 
-    // Skapa en händelse hanterare som även kan indikera att vi vill
-    // ta bort en bil ur listan...
-    // Använd Console.log för att indikera att vi tar bort en bil...
-    // Extra bonus! försök att ta bort den ur vår array...
-    tableRows = document.querySelectorAll('.table-container .delete');
-    // console.log(tableRows);
+  tableRows = document.querySelectorAll('.table-container .delete');
 
-    tableRows.forEach((item) => {
-      const id = item.parentNode.parentNode.children[1].firstChild.nodeValue;
-      // console.log(id);
+  tableRows.forEach((item) => {
+    const id = item.parentNode.parentNode.children[1].firstChild.nodeValue;
 
-      item.addEventListener('click', () => {
-        console.log(`Jag klickade för att ta bort bilen med id ${id}`);
-      });
+    item.addEventListener('click', () => {
+      // Kan va bra att ha en bekräftelse dialog rutan innan vi tar bort något...
+      // Skapa en modal dialogrutan
+      const vehicle = vehicleList.find((vehicle) => vehicle.id == id);
+      const index = vehicleList.findIndex((element) => element === vehicle);
+      // splice tar som första argument vilket index som vi skall starta på.
+      // som andra argument hur många element skall tas bort...
+      vehicleList.splice(index, 1);
+      createTable(vehicleList);
     });
   });
 }
 
 loadVehicles()
   .then((data) => {
-    console.log(data);
     createTable(data);
   })
   .catch((err) => console.log(err));
-
-// loadVehiclesFromRestApi()
-//   .then((data) => {
-//     console.log(data);
-//     createTable(data);
-//   })
-//   .catch((err) => console.log(err));
